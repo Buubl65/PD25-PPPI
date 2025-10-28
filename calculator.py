048230face464a0479e30522fd1fc07a68f9a692
Buubl65 <dima.sherbak12@gmail.com>
2025-10-28 23:19:31 +0200

    Fix

--- calculator.py
+++ calculator.py
@@ -1,17 +1,21 @@
-def add(a, b):
-    """Додає два числа"""
-    return a + b
-
-def subtract(a, b):
-    """Віднімає b від a"""
-    return a - b
-
-def multiply(a, b):
-    """Множить два числа"""
-    return a * b
-
-def divide(a, b):
-    """Ділить a на b (якщо b ≠ 0)"""
+def validate_divisor(b):
     if b == 0:
         raise ValueError("Ділення на нуль неможливе!")
-    return a / b
+
+class Calculator:
+
+    def add(a, b):
+        """Додає два числа"""
+        return a + b
+
+    def subtract(a, b):
+        """Віднімає b від a"""
+        return a - b
+
+    def multiply(a, b):
+        """Множить два числа"""
+        return a * b
+
+    def divide(a, b):
+        validate_divisor(b)
+        return a / b
